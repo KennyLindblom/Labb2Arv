534df4c4c75ee6d4d3c8f560528a126f9d1daba4
Kenny Lindblom <kennylindblom93@gmail.com>
2022-11-03 20:52:49 +0100

    Skapat basklass för projektet

---
+++ Labb2Arv/Animals.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb2Arv
+{
+     class Animals
+    {
+        public int Age;
+        public float Cost;
+        public string Name;
+        public string Gender;
+        public string Origin;
+
+        public Animals(int age = 0, float cost= 0, string name = "Unknown", string gender = "Female", string origin = "Unknown")
+        {
+            this.Age = age;
+            this.Cost = cost;
+            this.Name = name;
+            this.Gender = gender;
+            this.Origin = origin;
+        }
+
+        public void getGender()
+        {
+            Console.WriteLine($"{Gender} is the gender of this animal");
+
+        }
+        public void getName()
+        {
+            Console.WriteLine($" this animals name is {Name} ");
+
+        }
+
+        public void getOrigin()
+        {
+            Console.WriteLine($"{Name} is from {Origin} ");
+        }
+
+        public void getAge()
+        {
+            Console.WriteLine($"{Name} is {Age} Years old ");
+        }
+
+        public void getCost()
+        {
+            Console.WriteLine($"The animal {Name} has cost the zoo {Cost} to buy.... ");
+        }
+        
+
+    }
+}
--- Labb2Arv/Program.cs
+++ Labb2Arv/Program.cs
@@ -6,6 +6,7 @@ namespace Labb2Arv
     {
         static void Main(string[] args)
         {
+
             
             
         }
