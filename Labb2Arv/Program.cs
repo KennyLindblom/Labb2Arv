155553a60e05e1fae588ab329f7225de0e822997
Kenny Lindblom <kennylindblom93@gmail.com>
2022-11-05 17:33:44 +0100

    Skapade en switch case för att navigera programmet lättare och städa upp main

--- Labb2Arv/Program.cs
+++ Labb2Arv/Program.cs
@@ -6,66 +6,164 @@ namespace Labb2Arv
     {
         static void Main(string[] args)
         {
+            //Animals animals = new Animals();
+            //Lion lejon = new Lion();
+            //lejon.getName();
+            //lejon.getAge();
+            //lejon.getOrigin();
+            //lejon.makeSound();
+            //lejon.getCost();
+            //lejon.FavouritePray();
+            //Console.WriteLine("_________________________________________________________");
+
+            //Bear bear = new Bear();
+            //bear.makeSound();
+            //bear.getAge();
+            //bear.getOrigin();
+            //bear.getGender();
+            //bear.getCost();
+
+            //Console.WriteLine("__________________________________________________________");
+
+            //PolarBear polarbear = new PolarBear();
+            //polarbear.getName();
+            //polarbear.makeSound();
+            //polarbear.getGender();
+            //polarbear.getCost();
+            //polarbear.GetAttacks();
+            //polarbear.getOrigin();
+
+            //Console.WriteLine("____________________________________________________________");
+
+            //PandaBear pandaBear = new PandaBear();
+            //pandaBear.getName();
+            //pandaBear.GetHidingSpot();
+            //pandaBear.getOrigin();
+            //pandaBear.makeSound();
+
+
+            //Console.WriteLine("Register new animals to the zoo ?");
+            //Console.WriteLine();
+
+            //Console.WriteLine("How old is the animal? ");
+            //int addAnimalAge = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("How much did it cost to buy this animal? ");
+            //int addAnimalCost = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("What is the name of this animal? ");
+            //string addAnimalName = Console.ReadLine();
+            //Console.WriteLine("Is it a Male or a Female? ");
+            //string addGender = Console.ReadLine();
+            //Console.WriteLine("Where is this animal from? ");
+            //string addOrigin = Console.ReadLine();
+
+            //Animals newAnimal = new Animals(addAnimalAge, addAnimalCost, addAnimalName, addGender, addOrigin);
+            //newAnimal.newAnimalRegister();
+
+            mainMenu();
+
+
+
+
+
+
+
+
+        }
+
+        public static void mainMenu()
+        {
+            Console.WriteLine("1) Lion ");
+            Console.WriteLine("2) Bear ");
+            Console.WriteLine("3) PolarBear ");
+            Console.WriteLine("4) PandaBear ");
+            Console.WriteLine("5) Register new Animal ");
+
             Animals animals = new Animals();
-            Lion lejon = new Lion();
-            lejon.getName();
-            lejon.getAge();
-            lejon.getOrigin();
-            lejon.makeSound();
-            lejon.getCost();
-            lejon.FavouritePray();
-            Console.WriteLine("_________________________________________________________");
 
-            Bear bear = new Bear();
-            bear.makeSound();
-            bear.getAge();
-            bear.getOrigin();
-            bear.getGender();
-            bear.getCost();
+            int userChoiceMenu = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("__________________________________________________________");
+            switch(userChoiceMenu)
+            {
+                case 1:
+                    Lion lejon = new Lion();
+                    lejon.getName();
+                    lejon.getAge();
+                    lejon.getOrigin();
+                    lejon.makeSound();
+                    lejon.getCost();
+                    lejon.FavouritePray();
+                    Console.ReadKey();
+                    Console.Clear();
+                    mainMenu();
+                    break;
 
-            PolarBear polarbear = new PolarBear();
-            polarbear.getName();
-            polarbear.makeSound();
-            polarbear.getGender();
-            polarbear.getCost();
-            polarbear.GetAttacks();
-            polarbear.getOrigin();
+                    case 2:
+                    Bear bear = new Bear();
+                    bear.makeSound();
+                    bear.getAge();
+                    bear.getOrigin();
+                    bear.getGender();
+                    bear.getCost();
+                    Console.ReadKey();
+                    Console.Clear();
+                    mainMenu();
+                    break;
 
-            Console.WriteLine("____________________________________________________________");
+                    case 3:
+                    PolarBear polarbear = new PolarBear();
+                    polarbear.getName();
+                    polarbear.makeSound();
+                    polarbear.getGender();
+                    polarbear.getCost();
+                    polarbear.GetAttacks();
+                    polarbear.getOrigin();
+                    Console.ReadKey();
+                    Console.Clear();
+                    mainMenu();
 
-            PandaBear pandaBear = new PandaBear();
-            pandaBear.getName();
-            pandaBear.GetHidingSpot();
-            pandaBear.getOrigin();
-            pandaBear.makeSound();
+                    break;
 
+                case 4:
+                    PandaBear pandaBear = new PandaBear();
+                    pandaBear.getName();
+                    pandaBear.GetHidingSpot();
+                    pandaBear.getOrigin();
+                    pandaBear.makeSound();
+                    Console.ReadKey();
+                    Console.Clear();
+                    mainMenu();
+                    break;
 
-            Console.WriteLine("Register new animals to the zoo ?");
-            Console.WriteLine();
+                case 5:
 
-            Console.WriteLine("How old is the animal? ");
-            int addAnimalAge = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("How much did it cost to buy this animal? ");
-            int addAnimalCost = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("What is the name of this animal? ");
-            string addAnimalName = Console.ReadLine();
-            Console.WriteLine("Is it a Male or a Female? ");
-            string addGender = Console.ReadLine();
-            Console.WriteLine("Where is this animal from? ");
-            string addOrigin = Console.ReadLine();
+                    Console.WriteLine("Register new animals to the zoo ?");
+                    Console.WriteLine();
 
-            Animals newAnimal = new Animals(addAnimalAge, addAnimalCost, addAnimalName, addGender, addOrigin);
-            newAnimal.newAnimalRegister();
+                    Console.WriteLine("How old is the animal? ");
+                    int addAnimalAge = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("How much did it cost to buy this animal? ");
+                    int addAnimalCost = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("What is the name of this animal? ");
+                    string addAnimalName = Console.ReadLine();
+                    Console.WriteLine("Is it a Male or a Female? ");
+                    string addGender = Console.ReadLine();
+                    Console.WriteLine("Where is this animal from? ");
+                    string addOrigin = Console.ReadLine();
 
+                    Animals newAnimal = new Animals(addAnimalAge, addAnimalCost, addAnimalName, addGender, addOrigin);
+                    newAnimal.newAnimalRegister();
+                    Console.ReadKey();
+                    Console.Clear();
+                    mainMenu();
+                    break;
 
-
-
-
-
-
-
+                default:
+                    Console.WriteLine("Invalid choice"); 
+                    Console.ReadKey();
+                    Console.Clear();
+                    mainMenu();
+                    break;
+            }
         }
 
 
